ad1d0d3865aaefe0da05529f517f6de5784b8ba0
calin <calinoiu.alexandru@agilefreaks.com>
2020-01-25 22:01:34 +0200

    Stick to eml css for now

--- src/Signup.elm
+++ src/Signup.elm
@@ -1,8 +1,10 @@
-module Signup exposing (User)
+module Signup exposing (main)
 
+import Browser
 import Css exposing (..)
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (onClick, onInput)
 
 
 type alias User =
@@ -22,66 +24,94 @@ initialModel =
     }
 
 
-view : User -> Html msg
+view : User -> Html Msg
 view user =
-    div [ class "container" ]
-        [ div [ class "row" ]
-            [ div [ class "col-md-6 col-md-offset-3" ]
-                [ h1 [ class "text-center" ] [ text "Sign up" ]
-                , Html.form []
-                    [ div [ class "form-group" ]
-                        [ label
-                            [ class "control-label"
-                            , for "name"
-                            ]
-                            [ text "Name" ]
-                        , input
-                            [ class "form-control"
-                            , id "name"
-                            , type_ "text"
-                            , autofocus True
-                            ]
-                            []
-                        ]
-                    , div [ class "form-group" ]
-                        [ label
-                            [ class "control-label"
-                            , for "email"
-                            ]
-                            [ text "Email" ]
-                        , input
-                            [ class "form-control"
-                            , id "email"
-                            , type_ "email"
-                            ]
-                            []
-                        ]
-                    , div [ class "form-group" ]
-                        [ label
-                            [ class "control-label"
-                            , for "password"
-                            ]
-                            [ text "Password" ]
-                        , input
-                            [ class "form-control"
-                            , id "password"
-                            , type_ "password"
-                            ]
-                            []
-                        ]
-                    , div [ class "text-center" ]
-                        [ button
-                            [ class "btn btn-lg btn-primary"
-                            , type_ "submit"
-                            ]
-                            [ text "Create my account" ]
-                        ]
-                    ]
+    div []
+        [ h1 [ css [ paddingLeft (cm 3) ] ] [ text "Sign up" ]
+        , styledForm []
+            [ div []
+                [ text "Name"
+                , styledInput [ id "name", type_ "text", onInput SaveName ] []
+                ]
+            , div []
+                [ text "Email"
+                , styledInput [ id "email", type_ "email", onInput SaveEmail ] []
+                ]
+            , div []
+                [ text "Password"
+                , styledInput [ id "password", type_ "password", onInput SavePassword ] []
+                ]
+            , div []
+                [ styledButton [ type_ "submit", onClick Signup ]
+                    [ text "Create my account" ]
                 ]
             ]
         ]
 
 
-main : Html msg
+styledForm : List (Attribute msg) -> List (Html msg) -> Html msg
+styledForm =
+    styled Html.Styled.form
+        [ borderRadius (px 5)
+        , backgroundColor (hex "#f2f2f2")
+        , padding (px 20)
+        , Css.width (px 300)
+        ]
+
+
+styledInput : List (Attribute msg) -> List (Html msg) -> Html msg
+styledInput =
+    styled Html.Styled.input
+        [ display block
+        , Css.width (px 260)
+        , padding2 (px 12) (px 20)
+        , margin2 (px 8) (px 0)
+        , border (px 0)
+        , borderRadius (px 4)
+        ]
+
+
+styledButton : List (Attribute msg) -> List (Html msg) -> Html msg
+styledButton =
+    styled Html.Styled.button
+        [ Css.width (px 300)
+        , backgroundColor (hex "#397cd5")
+        , color (hex "#fff")
+        , padding2 (px 14) (px 20)
+        , marginTop (px 10)
+        , border (px 0)
+        , borderRadius (px 4)
+        , fontSize (px 16)
+        ]
+
+
+type Msg
+    = SaveName String
+    | SaveEmail String
+    | SavePassword String
+    | Signup
+
+
+update : Msg -> User -> User
+update msg user =
+    case msg of
+        SaveName name ->
+            { user | name = name }
+
+        SaveEmail email ->
+            { user | email = email }
+
+        SavePassword password ->
+            { user | password = password }
+
+        Signup ->
+            { user | loggedIn = True }
+
+
+main : Program () User Msg
 main =
-    view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = view >> toUnstyled
+        , update = update
+        }
